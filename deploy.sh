de9effa137295ebf9d13dde2d3d3317bb82f874f
Brian Loyal <bloyal@amazon.com>
2024-04-10 13:22:54 -0500

    Continue tweaking cicd

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-# 
+#
 
 ############################################################
-# Deploy the architecture for the Amazon HealthOmics Drug Discovery Workflows 
+# Deploy the architecture for the Amazon HealthOmics Drug Discovery Workflows
 # into your AWS account
 ## Options
-# -b S3 bucket name to use for deployment staging 
+# -b S3 bucket name to use for deployment staging
 # -n CloudFormation stack name
 # -r Deployment region
 # -w Wait for container build to complete before deployment?
@@ -21,26 +21,28 @@
 #   -w "T"
 
 set -e
-unset -v BUCKET_NAME STACK_NAME REGION TIMESTAMP WAITFORCONTAINER
+unset -v BUCKET_NAME ENVIRONMENT STACK_NAME REGION TIMESTAMP WAITFORCONTAINER
 
 TIMESTAMP=$(date +%s)
 
-while getopts 'b:n:r:w:' OPTION; do
-    case "$OPTION" in
-    b) BUCKET_NAME="$OPTARG";;
-    n) STACK_NAME="$OPTARG";;
-    r) REGION="$OPTARG";;
-    w) WAITFORCONTAINER="$OPTARG";;
-    *) exit 1 ;;    
-    esac
-done 
+while getopts 'b:e:n:r:w:' OPTION; do
+  case "$OPTION" in
+  b) BUCKET_NAME="$OPTARG" ;;
+  e) ENVIRONMENT="$OPTARG" ;;
+  n) STACK_NAME="$OPTARG" ;;
+  r) REGION="$OPTARG" ;;
+  w) WAITFORCONTAINER="$OPTARG" ;;
+  *) exit 1 ;;
+  esac
+done
 
+[ -z "$ENVIRONMENT" ] && { ENVIRONMENT="main"; }
 [ -z "$STACK_NAME" ] && { STACK_NAME="aho-ddw"; }
 [ -z "$REGION" ] && { REGION="us-east-1"; }
 [ -z "$WAITFORCONTAINER" ] && { WAITFORCONTAINER="N"; }
 
 zip -r build/code.zip * -x .\*/\*
-aws s3 cp build/code.zip s3://$BUCKET_NAME/build/code/code.zip
+aws s3 cp build/code.zip s3://$BUCKET_NAME/build/$ENVIRONMENT/code/code.zip
 rm build/code.zip
 
 aws cloudformation package --template-file build/cloudformation/root.yaml \
@@ -48,5 +50,6 @@ aws cloudformation package --template-file build/cloudformation/root.yaml \
   --s3-bucket $BUCKET_NAME --s3-prefix build/cloudformation
 aws cloudformation deploy --template-file build/cloudformation/packaged.yaml \
   --capabilities CAPABILITY_NAMED_IAM --stack-name $STACK_NAME --region $REGION \
-  --parameter-overrides S3BucketName=$BUCKET_NAME Timestamp=$TIMESTAMP WaitForContainerBuild=$WAITFORCONTAINER
+  --parameter-overrides S3BucketName=$BUCKET_NAME Timestamp=$TIMESTAMP \
+  WaitForContainerBuild=$WAITFORCONTAINER Environment=$ENVIRONMENT
 rm build/cloudformation/packaged.yaml
