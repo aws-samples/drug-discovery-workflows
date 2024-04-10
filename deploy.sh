2190e5cf2e02cd98ebb14f5a8e0b8dcc7da160de
Brian Loyal <bloyal@amazon.com>
2024-04-10 11:23:45 -0500

    Successful pyTorch build

--- deploy.sh
+++ deploy.sh
@@ -11,29 +11,33 @@
 # -b S3 bucket name to use for deployment staging 
 # -n CloudFormation stack name
 # -r Deployment region
+# -w Wait for container build to complete before deployment?
 #
 # Example CMD
 # ./deploy.sh \
 #   -b "my-deployment-bucket" \
 #   -n "my-aho-ddw-stack" \
 #   -r "us-east-1" \
+#   -w "T"
 
 set -e
-unset -v BUCKET_NAME STACK_NAME REGION TIMESTAMP
+unset -v BUCKET_NAME STACK_NAME REGION TIMESTAMP WAITFORCONTAINER
 
 TIMESTAMP=$(date +%s)
 
-while getopts 'b:n:r:' OPTION; do
+while getopts 'b:n:r:w:' OPTION; do
     case "$OPTION" in
     b) BUCKET_NAME="$OPTARG";;
     n) STACK_NAME="$OPTARG";;
-    r) REGION="$OPTARG";;       
+    r) REGION="$OPTARG";;
+    w) WAITFORCONTAINER="$OPTARG";;
     *) exit 1 ;;    
     esac
 done 
 
 [ -z "$STACK_NAME" ] && { STACK_NAME="aho-ddw"; }
-[ -z "$REGION" ] && { INPUT_FILE="us-east-1"; }
+[ -z "$REGION" ] && { REGION="us-east-1"; }
+[ -z "$WAITFORCONTAINER" ] && { WAITFORCONTAINER="N"; }
 
 zip -r build/code.zip * -x .\*/\*
 aws s3 cp build/code.zip s3://$BUCKET_NAME/build/code/code.zip
@@ -44,5 +48,5 @@ aws cloudformation package --template-file build/cloudformation/root.yaml \
   --s3-bucket $BUCKET_NAME --s3-prefix build/cloudformation
 aws cloudformation deploy --template-file build/cloudformation/packaged.yaml \
   --capabilities CAPABILITY_NAMED_IAM --stack-name $STACK_NAME --region $REGION \
-  --parameter-overrides S3BucketName=$BUCKET_NAME Timestamp=$TIMESTAMP
+  --parameter-overrides S3BucketName=$BUCKET_NAME Timestamp=$TIMESTAMP WaitForContainerBuild=$WAITFORCONTAINER
 rm build/cloudformation/packaged.yaml
