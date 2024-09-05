cf7541a6118df1dae0a91f1b8a9643f4ea8403ac
John Jacquay <john@bioteam.net>
2024-09-05 17:17:23 -0600

    Finished up testrun.sh

--- testrun.sh
+++ testrun.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
-# usage: ./testrun.sh <workflow_name>
+# usage: ./testrun.sh -w WORKFLOW_NAME -a ACCOUNT_ID -r REGION 
 
-set -e
-unset -v WORKFLOW_NAME ACCOUNT_ID REGION TIMESTAMP
+set -ex
+unset -v WORKFLOW_NAME ACCOUNT_ID REGION TIMESTAMP OMICS_EXECUTION_ROLE OUTPUT_BUCKET
 
 TIMESTAMP=$(date +%s)
 
 # set variables
-while getopts 'w:a:r:' OPTION; do
+while getopts 'w:a:r:o:b:' OPTION; do
   case "$OPTION" in
   w) WORKFLOW_NAME="$OPTARG" ;;
   a) ACCOUNT_ID="$OPTARG" ;;
   r) REGION="$OPTARG" ;;
+  o) OMICS_EXECUTION_ROLE="$OPTARG" ;;
+  b) OUTPUT_BUCKET="$OPTARG" ;;
   *) exit 1 ;;
   esac
 done
 
+# Check if the required variables are set
+if [ -z "$WORKFLOW_NAME" ] || [ -z "$ACCOUNT_ID" ] || [ -z "$REGION" ] || [ -z "$OMICS_EXECUTION_ROLE" ] || [ -z "$OUTPUT_BUCKET" ]; then
+  echo "Error: Missing required arguments."
+  echo "Usage: $0 -w WORKFLOW_NAME -a ACCOUNT_ID -r REGION"
+  exit 1
+fi
+
 # Login to ECR
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
-# build and push Docker image to ECR
+# Build and push Docker image to ECR
 docker build \
     --platform linux/amd64 \
     -t $WORKFLOW_NAME:latest \
     -f containers/$WORKFLOW_NAME/Dockerfile containers/$WORKFLOW_NAME
-docker tag $WORKFLOW_NAME:latest $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$WORKFLOW_NAME:latest
-docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$WORKFLOW_NAME:latest
+docker tag $WORKFLOW_NAME:latest $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$WORKFLOW_NAME:develop
+docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$WORKFLOW_NAME:develop
 
 # Package the workflow
 mkdir -p tmp/$WORKFLOW_NAME
@@ -37,11 +46,18 @@ sed -i='' "s/{{\s*\([A-Za-z0-9_-]*:[A-Za-z0-9_-]*\)\s*}}/$ACCOUNT_ID.dkr.ecr.$RE
 zip -j -r tmp/$WORKFLOW_NAME/workflow.zip tmp/$WORKFLOW_NAME
 
 # Create the workflow
-aws omics create-workflow --name $WORKFLOW_NAME-dev-$TIMESTAMP --region $REGION --cli-input-yaml file://tmp/$WORKFLOW_NAME/config.yaml --definition-zip fileb://tmp/$WORKFLOW_NAME/workflow.zip
+workflow_id=$(aws omics create-workflow --engine NEXTFLOW --name $WORKFLOW_NAME-dev-$TIMESTAMP --region $REGION --cli-input-yaml file://tmp/$WORKFLOW_NAME/config.yaml --definition-zip fileb://tmp/$WORKFLOW_NAME/workflow.zip --query 'id' --output text)
+aws omics wait workflow-active --region $REGION --id $workflow_id
 
 # Run the workflow
-# TODO
+aws omics start-run \
+    --retention-mode REMOVE \
+    --workflow-id $workflow_id \
+    --name $WORKFLOW_NAME-dev-$TIMESTAMP \
+    --role-arn "$OMICS_EXECUTION_ROLE" \
+    --parameters file://tmp/$WORKFLOW_NAME/params.json \
+    --region $REGION \
+    --output-uri s3://$OUTPUT_BUCKET/out
 
 # Cleanup
 rm -rf tmp/$WORKFLOW_NAME
-
